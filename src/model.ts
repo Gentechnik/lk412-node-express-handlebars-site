17c3a207bc03835ca4334bc05c462f0cdf606118
Lukas Karberg <deaderlp@googlemail.com>
2024-01-18 10:06:39 +0100

    Remove unused files and dependencies, update package.json

--- src/model.ts
+++ src/model.ts
@@ -1,21 +1,25 @@
-import axios from 'axios';
-import { IBook } from './interfaces';
+import axios from "axios";
+import { IBook } from "./interfaces";
+
+export const getVersionName = () => {
+	return "Handlebars 1.0";
+};
 
 export const getBooks = () => {
-	const bookUrl = 'https://edwardtanguay.vercel.app/share/books.json';
+	const bookUrl = "https://edwardtanguay.vercel.app/share/books.json";
 	return new Promise<IBook[]>(async (resolve, reject) => {
 		const response = await axios.get(bookUrl);
 		const rawBooks = response.data;
 		const books: IBook[] = [];
 		for (const rawBook of rawBooks) {
-			const book:IBook = {
+			const book: IBook = {
 				idCode: rawBook.idcode,
 				title: rawBook.title,
 				description: rawBook.description,
-				buyUrl: rawBook.buyurl
-			}
+				buyUrl: rawBook.buyurl,
+			};
 			books.push(book);
 		}
 		resolve(books);
-	})
-}
+	});
+};
--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,51 @@
-import express from 'express';
-import * as config from './config';
-import path from 'path';
-import * as model from './model';
+import express from "express";
+import * as config from "./config";
+import path from "path";
+import * as model from "./model";
+import { engine } from "express-handlebars";
 
 const app = express();
 const baseDir = process.cwd();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(baseDir, '/src/views'));
-app.use(express.static('public'));
+app.set("view engine", ".hbs");
+app.set("views", path.join(baseDir, "/src/views"));
 
-app.get('/', (req, res) => {
-	res.render('pages/welcome');
+app.engine(
+	".hbs",
+	engine({
+		extname: ".hbs",
+		layoutsDir: path.join(baseDir, "/src/views/layouts"),
+		partialsDir: path.join(baseDir, "/src/views/partials"),
+	})
+);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.render("pages/welcome", { versionName: model.getVersionName() });
 });
 
-app.get('/welcome', (req, res) => {
-	res.render('pages/welcome');
+app.get("/welcome", (req, res) => {
+	res.render("pages/welcome", { versionName: model.getVersionName() });
 });
 
-app.get('/books', async (req, res) => {
-	res.render('pages/books', { books: await model.getBooks() });
+app.get("/books", async (req, res) => {
+	res.render("pages/books", { books: await model.getBooks() });
 });
 
-app.get('/book/:idCode', async (req, res) => {
+app.get("/book/:idCode", async (req, res) => {
 	const idCode = req.params.idCode;
 	const books = await model.getBooks();
-	const book = books.find(m => m.idCode === idCode);
-	res.render('pages/book', { book });
+	const book = books.find((m) => m.idCode === idCode);
+	res.render("pages/book", { book });
 });
 
-app.get('/about', (req, res) => {
-	res.render('pages/about', {});
+app.get("/about", (req, res) => {
+	res.render("pages/about", {});
 });
 
-app.get('*', (req, res) => {
-	res.status(404).render('pages/404');
-})
+app.get("*", (req, res) => {
+	res.status(404).render("pages/404");
+});
 
 app.listen(config.getPort(), () => {
 	console.log(`Listening at http://localhost:${config.getPort()}`);
